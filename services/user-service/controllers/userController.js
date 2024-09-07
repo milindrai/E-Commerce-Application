c3eaad7f3ce0ef57e096c85e0cc019eabb107979
milindrai <milindrai365@gmail.com>
2024-09-07 18:05:06 +0530

    Update dependencies and routes for user authentication

--- services/user-service/controllers/userController.js
+++ services/user-service/controllers/userController.js
@@ -1,4 +1,7 @@
 const User=require('../models/User');
+const bcrypt = require('bcryptjs');
+const env=require('dotenv');
+const jwt=require('jsonwebtoken');
 
 const registerUser=async(req,res)=>{
     const {name,email,password}=req.body;
@@ -15,6 +18,17 @@ const registerUser=async(req,res)=>{
             password
         });
         await user.save();
+
+        // Generate JWT token
+        const token=jwt.sign({userId:user._id},process.env.JWT_SECRET);
+
+        // Store the token in a cookie
+        res.cookie('token', token, {
+            httpOnly: true, // The cookie is only accessible by the web server
+            secure: process.env.NODE_ENV === 'production', // Use secure cookies in production environment
+            sameSite: 'strict', // Strictly same site
+        });
+        
         res.status(201).json({message:'User registered successfully'});
     }
     catch(err){
@@ -23,4 +37,28 @@ const registerUser=async(req,res)=>{
     }
 }
 
-module.exports={registerUser};
+
+const loginUser=async(req,res)=>{
+    const {email,password}=req.body;
+    try{
+        let user=await User.findOne({email});
+        if(!user){
+            return res.status(400).json({message:'Invalid email'});
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch){
+            return res.status(400).json({message:'Invalid email or password'});
+        }
+        const token=jwt.sign({userId:user._id},process.env.JWT_SECRET);
+        res.json({message:'Login successful'});
+        
+    }
+    catch(err){
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+}
+
+
+
+module.exports={registerUser,loginUser};
--- services/user-service/models/User.js
+++ services/user-service/models/User.js
@@ -1,4 +1,6 @@
 const mongoose=require('mongoose');
+const bcrypt=require('bcryptjs');
+
 
 const UserSchema=new mongoose.Schema({
     name:{
@@ -14,6 +16,19 @@ const UserSchema=new mongoose.Schema({
         type:String,
         required:true
     }
-})
+});
+
+UserSchema.pre('save', async function(next) {
+    // Only hash the password if it has been modified (or is new)
+    if (!this.isModified('password')) return next();
+
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (err) {
+        next(err);
+    }
+});
 
 module.exports=mongoose.model('User',UserSchema);

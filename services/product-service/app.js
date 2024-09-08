75e0d533a9aef9169a92f4d7d8fd2e22e0e03f9d
milindrai <milindrai365@gmail.com>
2024-09-08 14:27:25 +0530

    Update product-service

---
+++ services/product-service/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const db=require('./config/db');
+const app = express();
+const productRoutes = require('./routes/productRoutes');
+const mongoose  = require('mongoose');
+require('dotenv').config();
+
+app.use(express.json());
+
+app.use('/api/products',productRoutes);
+
+const PORT=process.env.PORT || 5002;
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(()=>console.log('Database connected'))
+    .catch(err=>console.error('Database connection error',err));
+
+app.listen(PORT, () => {
+    console.log(`Product-service is running on port ${PORT}`);
+});

aa9ff19448b8e72f1b505dcf0a0479cb19166b05
milindrai <milindrai365@gmail.com>
2024-09-08 21:41:31 +0530

    Add authentication middleware for product routes

--- services/product-service/routes/productRoutes.js
+++ services/product-service/routes/productRoutes.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const {createProduct, getProducts, updateDetails, deleteProduct, searchProduct, addReview, getReviews, deleteReview,upload,uploadProductImage, getProductImage, deleteProductImage} = require('../controllers/productController');
+const authenticate = require('../middleware/authenticate');
 
 
-router.post('/create',createProduct);
-router.get('/details', getProducts);
-router.put('/update/:id', updateDetails);
-router.delete('/delete/:id', deleteProduct);
-router.get('/search',searchProduct);
-router.post('/:id/review',addReview);
-router.get('/:id/reviews',getReviews);
-router.delete('/:productId/reviews/:reviewId', deleteReview);
-router.post('/:productId/image', upload.single('image'), uploadProductImage);
-router.get('/:productId/image', getProductImage);
-router.delete('/:productId/image', deleteProductImage);
+router.post('/create',authenticate,createProduct);
+router.get('/details',authenticate, getProducts);
+router.put('/update/:id',authenticate, updateDetails);
+router.delete('/delete/:id',authenticate, deleteProduct);
+router.get('/search',authenticate,searchProduct);
+router.post('/:id/review',authenticate,addReview);
+router.get('/:id/reviews',authenticate,getReviews);
+router.delete('/:productId/reviews/:reviewId', authenticate,deleteReview);
+router.post('/:productId/image', upload.single('image'), authenticate,uploadProductImage);
+router.get('/:productId/image',authenticate, getProductImage);
+router.delete('/:productId/image',authenticate, deleteProductImage);
 
 module.exports = router;
